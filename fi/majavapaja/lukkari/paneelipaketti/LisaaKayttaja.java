a400cb6e4b273087a424186f17ec5aacabb0bc0b
Germaanisoturi <german.lintula@me.com>
2012-12-05 12:48:42 +0200

    null

--- fi/majavapaja/lukkari/paneelipaketti/LisaaKayttaja.java
+++ fi/majavapaja/lukkari/paneelipaketti/LisaaKayttaja.java
@@ -1,17 +1,13 @@
 package fi.majavapaja.lukkari.paneelipaketti;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.UUID;
 
 import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -19,25 +15,30 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
+import fi.majavapaja.lukkari.Database;
+import fi.majavapaja.lukkari.Kayttajatunnus;
+import fi.majavapaja.lukkari.Oppilas;
 import fi.majavapaja.lukkari.Paaikkuna;
+import fi.majavapaja.lukkari.Ryhma;
 
 public class LisaaKayttaja extends JPanel implements ActionListener {
-	private JLabel lblNewLabel = new JLabel("Käyttäjätunnus:");
+	private JLabel lblKayttajatunnus = new JLabel("Käyttäjätunnus:");
 	private JLabel lblSalasana = new JLabel("Salasana:");
 	private JLabel lblSalasanaUudestaan = new JLabel("Uudestaan:");
 	private JLabel lblOikeudet = new JLabel("Oikeudet:");
 	private JLabel lblEtunimi = new JLabel("Etunimi:");
 	private JLabel lblSukunimi = new JLabel("Sukunimi:");
 	private JLabel lblRyhm = new JLabel("Ryhmä:");
-	private JTextField textField;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
+	private JTextField tfKayttajatunnus;
+	private JTextField tfEtunimi;
+	private JTextField tfSukunimi;
+	private JPasswordField pfSalasana;
+	private JPasswordField pfSalasanaUudestaan;
 	private JRadioButton rdbtnOppilas = new JRadioButton("Oppilas");
 	private JRadioButton rdbtnYllpito = new JRadioButton("Ylläpito");
-	private String[] stuff = { "ÇŒÆØﬂßƒﬁ", "Ç" };
-	private JComboBox comboBox;
+	private int Oikeudet = 0;
+	private Ryhma[] stuff;
+	private JComboBox<Ryhma> comboBox;
 	private JButton btnLis = new JButton("Lisää");
 	private JButton btnPalaa = new JButton("Takaisin");
 	private JButton ÇŒÆØﬂßƒﬁ = new JButton("       ");
@@ -54,8 +55,8 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 		
 		setSize(800, 600);
 
-		textField = new JTextField();
-		textField.setColumns(10);
+		tfKayttajatunnus = new JTextField();
+		tfKayttajatunnus.setColumns(10);
 
 		ButtonGroup bg = new ButtonGroup();
 
@@ -69,11 +70,11 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 		rdbtnOppilas.addActionListener(this);
 		rdbtnYllpito.addActionListener(this);
 
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
+		tfEtunimi = new JTextField();
+		tfEtunimi.setColumns(10);
 
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
+		tfSukunimi = new JTextField();
+		tfSukunimi.setColumns(10);
 
 		btnLis.setFocusable(false);
 		btnLis.addActionListener(this);
@@ -88,11 +89,12 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 		ÇŒÆØﬂßƒﬁ.setContentAreaFilled(false);
 		ÇŒÆØﬂßƒﬁ.setOpaque(false);
 
-		passwordField = new JPasswordField();
+		pfSalasana = new JPasswordField();
 
-		passwordField_1 = new JPasswordField();
+		pfSalasanaUudestaan = new JPasswordField();
 
-		comboBox = new JComboBox<String>(stuff);
+		stuff = Database.getRyhmat().toArray(new Ryhma[Database.getRyhmat().size()]);
+		comboBox = new JComboBox<Ryhma>(stuff);
 
 		
 		GroupLayout groupLayout = new GroupLayout(this);
@@ -108,22 +110,22 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 								.addComponent(lblEtunimi))
 							.addGap(44))
 						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addComponent(lblNewLabel)
+							.addComponent(lblKayttajatunnus)
 							.addComponent(lblSalasana, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
 							.addComponent(lblSalasanaUudestaan, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
 							.addComponent(lblOikeudet)))
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(textField, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
-						.addComponent(textField_2, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+						.addComponent(tfKayttajatunnus, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+						.addComponent(tfEtunimi, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(rdbtnOppilas)
 							.addGap(18)
 							.addComponent(rdbtnYllpito))
-						.addComponent(textField_3, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
-						.addComponent(passwordField, 221, 250, Short.MAX_VALUE)
-						.addComponent(passwordField_1, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+						.addComponent(tfSukunimi, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+						.addComponent(pfSalasana, 221, 250, Short.MAX_VALUE)
+						.addComponent(pfSalasanaUudestaan, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
 						.addComponent(comboBox, 0, 250, Short.MAX_VALUE)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(btnLis, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
@@ -140,15 +142,15 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(134)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel))
+						.addComponent(tfKayttajatunnus, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblKayttajatunnus))
 					.addGap(14)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(pfSalasana, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblSalasana))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(passwordField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(pfSalasanaUudestaan, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblSalasanaUudestaan))
 					.addGap(40)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
@@ -160,11 +162,11 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 							.addPreferredGap(ComponentPlacement.RELATED)))
 					.addGap(24)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(tfEtunimi, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblEtunimi))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(tfSukunimi, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblSukunimi))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
@@ -182,44 +184,45 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 
 	}
 	
-	/*public static void main(String[] args) {
-		LisaaKayttaja l = new LisaaKayttaja(paaikkuna);
-
-		JFrame frame = new JFrame("blah");
-		frame.getContentPane().add(l);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}*/
-
+	private String ripoffPassword(char[] password) {
+		String jotain = "";
+		for (int i = 0; i < password.length; i++) {
+			jotain += password[i];
+		}
+		return jotain;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (rdbtnOppilas == e.getSource()) {
-			textField_2.setVisible(true);
-			textField_3.setVisible(true);
+			tfEtunimi.setVisible(true);
+			tfSukunimi.setVisible(true);
 			comboBox.setVisible(true);
 			lblEtunimi.setVisible(true);
 			lblSukunimi.setVisible(true);
 			lblRyhm.setVisible(true);
 		} else if (rdbtnYllpito == e.getSource()) {
-			textField_2.setVisible(false);
-			textField_3.setVisible(false);
+			tfEtunimi.setVisible(false);
+			tfSukunimi.setVisible(false);
 			comboBox.setVisible(false);
 			lblEtunimi.setVisible(false);
 			lblSukunimi.setVisible(false);
 			lblRyhm.setVisible(false);
 		} else if (btnLis == e.getSource()) {
-			System.out.println("us: " + textField.getText() + " pw1: "
-					+ passwordField.getText() + " pw2: "
-					+ passwordField_1.getText() + " fna: "
-					+ textField_2.getText() + " lna: " + textField_3.getText());
+			
+			Kayttajatunnus k = new Kayttajatunnus(tfEtunimi.getText(), ripoffPassword(pfSalasana.getPassword()), Oikeudet);
+			Oppilas o = new Oppilas(tfEtunimi.getText(), tfSukunimi.getText(), (Ryhma)comboBox.getSelectedItem());
+			
+			System.out.println("us: " + tfKayttajatunnus.getText() + " pw1: "
+					+ ripoffPassword(pfSalasana.getPassword()) + " pw2: "
+					+ripoffPassword(pfSalasanaUudestaan.getPassword()) + " fna: "
+					+ tfEtunimi.getText() + " lna: " + tfSukunimi.getText());
 		} else if (btnPalaa == e.getSource()) {
 			paaikkuna.edellinenPaneeli();
 			System.out.println("blaa");
 		} else if (ÇŒÆØﬂßƒﬁ == e.getSource()) {
-			if (comboBox.getSelectedItem().equals("Ç") && rdbtnYllpito.isSelected()) {
+			Ryhma r = (Ryhma) comboBox.getSelectedItem();
+			if (r.getNimi().equals("Ç") && rdbtnYllpito.isSelected()) {
 				System.out.println("vblah");
 				new Thread() {
 					int asd = 0;
@@ -231,13 +234,13 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 								asd = 0;
 							String text = blah[asd];
 
-							textField.setText(text);
-							textField_2.setText(text);
-							textField_3.setText(text);
-							passwordField.setText(text);
-							passwordField_1.setText(text);
+							tfKayttajatunnus.setText(text);
+							tfEtunimi.setText(text);
+							tfSukunimi.setText(text);
+							pfSalasana.setText(text);
+							pfSalasanaUudestaan.setText(text);
 								lblEtunimi.setText(text);
-								lblNewLabel.setText(text);
+								lblKayttajatunnus.setText(text);
 								lblOikeudet.setText(text);
 								lblRyhm.setText(text);
 								lblSalasana.setText(text);
@@ -246,7 +249,6 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 								rdbtnOppilas.setText(text);
 								rdbtnYllpito.setText(text);
 								comboBox.removeAllItems();
-								comboBox.addItem(text);
 								btnLis.setText(text);
 								btnPalaa.setText(text);
 
@@ -260,11 +262,11 @@ public class LisaaKayttaja extends JPanel implements ActionListener {
 				}.start();
 			} else {
 				System.out.println("ÇŒÆØﬂßƒﬁ");
-				textField.setText("ÇŒÆØﬂßƒﬁ");
-				textField_2.setText("ÇŒÆØﬂßƒﬁ");
-				textField_3.setText("ÇŒÆØﬂßƒﬁ");
-				passwordField.setText("ÇŒÆØﬂßƒﬁ");
-				passwordField_1.setText("ÇŒÆØﬂßƒﬁ");
+				tfKayttajatunnus.setText("ÇŒÆØﬂßƒﬁ");
+				tfEtunimi.setText("ÇŒÆØﬂßƒﬁ");
+				tfSukunimi.setText("ÇŒÆØﬂßƒﬁ");
+				pfSalasana.setText("ÇŒÆØﬂßƒﬁ");
+				pfSalasanaUudestaan.setText("ÇŒÆØﬂßƒﬁ");
 
 			}
 		}
