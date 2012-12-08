b1f85dffe2dca21eed01b72e4536071a5bee093a
Henry Heikkinen <rce@rce.fi>
2012-12-08 14:30:16 +0200

    Korjaa bugi oppilaanmuokkauspaneelissa

--- fi/majavapaja/lukkari/Ryhma.java
+++ fi/majavapaja/lukkari/Ryhma.java
@@ -9,7 +9,32 @@ package fi.majavapaja.lukkari;
  * @author s1001069
  */
 public class Ryhma {
-    private String nimi;
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nimi == null) ? 0 : nimi.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ryhma other = (Ryhma) obj;
+		if (nimi == null) {
+			if (other.nimi != null)
+				return false;
+		} else if (!nimi.equals(other.nimi))
+			return false;
+		return true;
+	}
+
+	private String nimi;
     private int id;
     
     public Ryhma(String nimi) {
