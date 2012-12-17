323ecc762e16fb0929954e5bae1ee400d47d2bdd
Henry Heikkinen <rce@rce.fi>
2012-12-17 16:14:31 +0200

    EtsiKurssi: Lisää javadoc

--- fi/majavapaja/lukkari/paneelipaketti/EtsiKurssi.java
+++ fi/majavapaja/lukkari/paneelipaketti/EtsiKurssi.java
@@ -34,6 +34,11 @@ public class EtsiKurssi extends JPanel {
 	private Paaikkuna ikkuna;
 	private JList<Kurssi> kurssiList;
 	
+	/**
+	 * Luo kurssinhakunäkymän.
+	 * 
+	 * @param ikkuna Paaikkuna, johon paneeli sijoitetaan.
+	 */
 	public EtsiKurssi(Paaikkuna ikkuna) {
 		this.ikkuna = ikkuna;
 		setPreferredSize(new Dimension(800, 600));
@@ -123,6 +128,10 @@ public class EtsiKurssi extends JPanel {
 		setLayout(groupLayout);
 	}
 	
+	/**
+	 * Vaihtaa näkymäksi uuden MuokkaaKurssia paneelin, jos
+	 * hakutuloksista on valittuna ryhmä.
+	 */
 	protected void muokkaaActionPerformed() {
 		int i = kurssiList.getSelectedIndex();
 		if (i == -1)
@@ -131,10 +140,17 @@ public class EtsiKurssi extends JPanel {
 		ikkuna.vaihdaPaneeli(new MuokkaaKurssia(ikkuna, kurssi));
 	}
 	
+	/**
+	 * Pyytää pääikkunaa palaamaan edelliseen paneeliin.
+	 */
 	protected void takaisinActionPerformed() {
 		ikkuna.edellinenPaneeli();
 	}
 	
+	/**
+	 * Hakee tietokannasta syötettyä nimeä vastaavat kurssit ja
+	 * lisää ne hakutulosten listaan.
+	 */
 	protected void haeActionPerformed() {
 		String nimi = kurssinNimiField.getText().trim();
 		
