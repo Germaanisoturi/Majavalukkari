3f1798a7fe2c44d4c043440a1198aa04ef8ea5f6
AlexEngblom <alex.engblomm@gmail.com>
2012-12-19 10:22:57 +0200

    Käyttäjätunnus javadocitettuna ja valmiina!

--- fi/majavapaja/lukkari/Kayttajatunnus.java
+++ fi/majavapaja/lukkari/Kayttajatunnus.java
@@ -2,77 +2,116 @@ package fi.majavapaja.lukkari;
 
 /**
  * Käyttäjätunnus toimii tiedonvälittäjänä käyttäjien tietoja varten.
- * 
+ *
  * @author Majavapaja
  */
 public class Kayttajatunnus {
-	public static final int OPPILAS = 0;
-	public static final int YLLAPITAJA = 1;
-	
-	private String kayttajatunnus;
-	private String salasana;
-	private int id;
-	private Oppilas oppilas;
-	
-        /**
-         * 
-         * 
-         * @param kayttajatunnus käyttäjänimi.
-         * @param salasana käyttäjän salasana.
-         * @param oikeudet käyttäjän oikeu
-         * @param oppilas 
-         */
-	public Kayttajatunnus(String kayttajatunnus, String salasana, Oppilas oppilas) {
-		this(kayttajatunnus, salasana, oppilas, -1);
-	}
-	
-	// TODO: Poista tämä käytöstä.
-//	public Kayttajatunnus(String kayttajatunnus, String salasana) {
-//		this(kayttajatunnus, salasana, null, -1);
-//	}
-	
-	public Kayttajatunnus(String kayttajatunnus, String salasana, Oppilas oppilas, int id) {
-		this.kayttajatunnus = kayttajatunnus;
-		this.salasana = salasana;
-		this.oppilas = oppilas;
-		this.id = id;
-	}
-	
-	public int getId() {
-		return id;
-	}
 
-	public String getKayttajanimi() {
-		return kayttajatunnus;
-	}
-	
-	public void setKayttajnimi(String kayttajanimi) {
-		this.kayttajatunnus = kayttajanimi;
-	}
+    public static final int OPPILAS = 0;
+    public static final int YLLAPITAJA = 1;
+    private String kayttajanimi;
+    private String salasana;
+    private int id;
+    private Oppilas oppilas;
 
-	public String getSalasana() {
-		return salasana;
-	}
-	
-	public void setSalasana(String salasana) {
-		this.salasana = salasana;
-	}
+    /**
+     * Luo käyttäjätunnuksen.
+     *
+     * @param kayttajatunnus käyttäjänimi.
+     * @param salasana käyttäjän salasana.
+     * @param oppilas oppilaan tiedot.
+     */
+    public Kayttajatunnus(String kayttajatunnus, String salasana, Oppilas oppilas) {
+        this(kayttajatunnus, salasana, oppilas, -1);
+    }
 
-	public int getOikeudet() {
-		if (oppilas == null){
-                    return YLLAPITAJA;
-                } else {
-                    return OPPILAS;
-                }
-	}
-	
-	public Oppilas getOppilas() {
-		return oppilas;
-	}
-	
-	public String toString() {
-		if (oppilas == null)
-			return kayttajatunnus;
-		return String.format("%s (%s)", kayttajatunnus, oppilas);
-	}
+    /**
+     * Luo käyttäjätunnuksen id:llä varustettuna tietokannan päivityksiä varten.
+     *
+     * @param kayttajatunnus käyttäjänimi.
+     * @param salasana käyttäjän salasana.
+     * @param oppilas oppilaan tiedot.
+     * @param id oppilaan pääavain kannassa.
+     */
+    public Kayttajatunnus(String kayttajatunnus, String salasana, Oppilas oppilas, int id) {
+        this.kayttajanimi = kayttajatunnus;
+        this.salasana = salasana;
+        this.oppilas = oppilas;
+        this.id = id;
+    }
+
+    /**
+     * Palauttaa käyttäjätunnuksen kannan pääavaimen.
+     *
+     * @return käyttäjätunnuksen kannan pääavain.
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Palauttaa käyttäjän tunnuksen.
+     *
+     * @return käyttäjätunnuksen nimi.
+     */
+    public String getKayttajanimi() {
+        return kayttajanimi;
+    }
+
+    /**
+     * Asettaa käyttäjätunnukselle uuden salasanan.
+     *
+     * @param kayttajanimi asetettava käyttäjänimi.
+     */
+    public void setKayttajnimi(String kayttajanimi) {
+        this.kayttajanimi = kayttajanimi;
+    }
+
+    /**
+     * Palauttaa käyttäjän salasanan.
+     *
+     * @return käyttäjän salasana.
+     */
+    public String getSalasana() {
+        return salasana;
+    }
+
+    /**
+     * Asettaa käyttäjätunnukselle uuden salasanan.
+     *
+     * @param salasana asetettava salasana.
+     */
+    public void setSalasana(String salasana) {
+        this.salasana = salasana;
+    }
+
+    /**
+     * Palauttaa int arvon sen mukaan onko käyttäjä oppilas vai ei.
+     *
+     * @return käyttäjän oikeudet.
+     */
+    public int getOikeudet() {
+        if (oppilas == null) {
+            return YLLAPITAJA;
+        } else {
+            return OPPILAS;
+        }
+    }
+
+    /**
+     * Palauttaa käyttäjän oppilastiedot omassa dataoliossaan.
+     *
+     * @return oppilaan tiedot.
+     */
+    public Oppilas getOppilas() {
+        return oppilas;
+    }
+
+    @Override
+    public String toString() {
+        if (oppilas == null) {
+            return kayttajanimi;
+        }
+        return String.format("%s (%s)", kayttajanimi, oppilas);
+    }
 }
